36ca3dbbb5afb7e5d6289de74984b6756fd7705a
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-11 10:33:38 -0800

    Add solution to findPairs that involves generating subarrays and filtering

---
+++ DSABook/findPairs.js
@@ -0,0 +1,35 @@
+// Given a list of numbers,
+// find two numbers in the list that add up to ten
+// and return them. If no such pair exists, return null.
+
+// It is guaranteed that there is either exactly one pair of numbers
+// that satisfies the condition, or no pairs at all.
+
+// Test Cases:
+
+function generatePairs(array) {
+  let pairs = [];
+  for (let i = 0; i < array.length - 1; i += 1) {
+    for (let j = i + 1; j < array.length; j += 1) {
+      pairs.push([array[i], array[j]]);
+    }
+  }
+  return pairs;
+}
+
+function findPair(array) {
+  let pairs = generatePairs(array);
+  let pair = pairs.filter(([n1, n2]) => addToTen(n1, n2));
+  if (pair.length === 0) return null;
+  return pair.flat();
+}
+
+function addToTen(num1, num2) {
+  return (num1 + num2) === 10;
+}
+
+console.log(findPair([2, 3, 9, 7])); // Output: [3, 7]
+console.log(findPair([10, 6, -1, 2])); // null
+console.log(findPair([1, 2, 5, 6])); // null
+console.log(findPair([1, 3, 6, 10, 4, 5])); // [6, 4]
+console.log(findPair([4, -5, 3, 15, 5])); // [-5, 15]
