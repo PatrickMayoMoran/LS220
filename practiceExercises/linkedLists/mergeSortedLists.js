fab3e249e2af94b3f1ed982c0a9099c4d65889f9
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-18 14:28:26 -0800

    Add initial solution to mergeSortedLists problem

---
+++ practiceExercises/linkedLists/mergeSortedLists.js
@@ -0,0 +1,70 @@
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+function createLinkedList(arr) {
+  let head = new ListNode(0);
+  let current = head;
+  arr.forEach(val => {
+    current.next = new ListNode(val);
+    current = current.next;
+  });
+  return head.next;
+}
+function printLinkedList(head) {
+  let current = head;
+  let listStr = '';
+  while (current !== null) {
+    listStr += current.val + ' -> ';
+    current = current.next;
+  }
+  listStr += 'null';
+  console.log(listStr);
+}
+
+function mergeSortedLists(head1, head2) {
+  let newHead = new ListNode(0);
+  let current = newHead;
+  let current1 = head1;
+  let current2 = head2;
+
+  while (current1 !== null && current2 !== null) {
+      if (current1.val < current2.val) {
+        current.next = current1;
+        current1 = current1.next;
+      } else {
+        current.next = current2;
+        current2 = current2.next;
+      }
+      current = current.next;
+    }
+
+    if (current1 === null) {
+      current.next = current2;
+    } else {
+      current.next = current1;
+    }
+
+  return newHead.next;
+}
+
+let list1 = createLinkedList([1, 3, 5]);
+let list2 = createLinkedList([2, 4, 6]);
+printLinkedList(mergeSortedLists(list1, list2)); // Expected: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+
+let list3 = createLinkedList([1, 2, 3]);
+let list4 = createLinkedList([]);
+printLinkedList(mergeSortedLists(list3, list4)); // Expected: 1 -> 2 -> 3 -> null
+
+let list5 = createLinkedList([]);
+let list6 = createLinkedList([1]);
+printLinkedList(mergeSortedLists(list5, list6)); // Expected: 1 -> null
+
+let list7 = createLinkedList([1, 5, 9]);
+let list8 = createLinkedList([2, 4, 6, 8, 10]);
+printLinkedList(mergeSortedLists(list7, list8)); // Expected: 1 -> 2 -> 4 -> 5 -> 6 -> 8 -> 9 -> 10 -> null
+
+let list9 = createLinkedList([1, 2, 5]);
+let list10 = createLinkedList([3, 6, 7]);
+printLinkedList(mergeSortedLists(list9, list10)); // Expected: 1 -> 2 -> 3 -> 5 -> 6 -> 7 -> null
