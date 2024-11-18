8601f43817b400457d414182423432e7e77d4b83
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-18 11:10:48 -0800

    Add provided solution to isSquareInteger using explicit binary search over the range of possible numbers

--- practiceExercises/binarySearch/isSquareInteger.js
+++ practiceExercises/binarySearch/isSquareInteger.js
@@ -1,6 +1,28 @@
+/*
+ORIGINAL SOLUTION - not binary search...
 function isSquareInteger(number) {
   for (let i = 1; i*i <= number; i++) {
     if (i * i === number) return true;
+  }
+
+  return false;
+}
+*/
+
+function isSquareInteger(num) {
+  let left = 0;
+  let right = num;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let product = mid * mid;
+    if (product === num) {
+      return true;
+    } else if (product < num) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
 
   return false;
