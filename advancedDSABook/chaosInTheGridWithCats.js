786574c55d583d7ca8a57a8e1db485c386566e71
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-21 18:27:38 -0800

    Add prompt for chaos in the grid with cats problem

---
+++ advancedDSABook/chaosInTheGridWithCats.js
@@ -0,0 +1,68 @@
+// You are given a grid represented as a
+// nested array filled with strings.
+
+// Chaos is standing at the top-left corner of
+// the grid and can move either down or right at
+// any point in time. However, there are sleeping
+// cats in certain squares, represented by the
+// letter "C" in the grid, and Chaos cannot go through
+// these squares.
+
+// Determine the number of distinct paths Chaos
+// can take to reach a bowl of treats placed at
+// the bottom-right corner of the grid.
+
+// Define a function `chaosInTheGridWithCats` that,
+// given a nested array, returns the number of
+// unique paths that Chaos can take to reach the
+//  bottom-right corner.
+
+// The grid will have at least 1 row and 1 column.
+
+// Example:
+
+// Given the following 2x3 grid:
+
+const grid = [
+  ["", "C", ""],
+  ["", "", ""],
+];
+
+// There is only one distinct path Chaos can take:
+// 1. Down -> Right -> Right
+
+function chaosInTheGridWithCats(grid) {
+  // implementation
+}
+
+// Test Cases:
+
+const grid1 = [
+  ["", "C"],
+  ["", ""],
+];
+const grid2 = [["", "C"]];
+const grid3 = [
+  ["", "", ""],
+  ["", "C", ""],
+  ["", "", ""],
+];
+const grid4 = [
+  ["", "", "", "", "C"],
+  ["", "C", "", "", ""],
+  ["", "", "", "C", ""],
+];
+const grid5 = [
+  ["", "", "", "", "C", ""],
+  ["", "C", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "C", "", ""],
+  ["", "C", "", "", "", ""],
+  ["", "", "", "", "", ""],
+];
+
+console.log(chaosInTheGridWithCats(grid1) === 1);
+console.log(chaosInTheGridWithCats(grid2) === 0);
+console.log(chaosInTheGridWithCats(grid3) === 2);
+console.log(chaosInTheGridWithCats(grid4) === 2);
+console.log(chaosInTheGridWithCats(grid5) === 43);
