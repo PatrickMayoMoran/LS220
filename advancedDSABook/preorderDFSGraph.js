85a532390b042f0ae5ad82ae6ffd388ba50dbe4e
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-25 09:10:35 -0800

    Add initial solution to preorder depth first search of graph problem

---
+++ advancedDSABook/preorderDFSGraph.js
@@ -0,0 +1,33 @@
+// Implement a function `dfs` that accepts two arguments: the adjacency list
+// representing a directed acyclic graph and a starting vertex (source).
+// The function should print the vertices in preorder depth-first
+// traversal order.
+
+function dfs(adjList, source) {
+  const stack = [];
+  const result = [];
+
+  stack.push(source);
+  while (stack.length > 0) {
+    let current = stack.pop();
+    result.push(current);
+    stack.push(...adjList.get(current));
+  }
+
+  result.forEach(e => console.log(e));
+}
+
+const adjList = new Map();
+adjList.set(1, []);
+adjList.set(2, [1, 3, 4]);
+adjList.set(3, [5]);
+adjList.set(4, [6]);
+adjList.set(5, []);
+adjList.set(6, []);
+adjList.set(7, [6]);
+
+dfs(adjList, 2); // 2, 4, 6, 3, 5, 1 or 2, 1, 3, 5, 4, 6
+
+// Note that the output can vary based on the order in
+// which you process neighbors. These two outputs are
+// the most likely, but other valid orders exist.
