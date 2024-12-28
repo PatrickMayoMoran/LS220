20c04626a5aa465c31307c4ac7494a80e72f7590
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-12-28 13:57:39 -0800

    Add example of using binary search to find zero or zero position in array

---
+++ DSAredo/binarySearch/findZero.js
@@ -0,0 +1,20 @@
+function findZeroPosition(array) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (array[mid] === 0) {
+      return mid;
+    } else if (array[mid] < 0) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
+
+console.log(findZeroPosition([-7, -5, -3, 0, 2])); // 3
+console.log(findZeroPosition([3,5,7,9,11])); // 0
