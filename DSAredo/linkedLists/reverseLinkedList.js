b4d6bcabce936594a54567a2424131e3227502b3
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-12-29 14:00:22 -0800

    Add implementation of reversing a linked list

---
+++ DSAredo/linkedLists/reverseLinkedList.js
@@ -0,0 +1,44 @@
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+function reverseLinkedList(head) {
+  let prev = null;
+  let curr = head;
+
+  if (!head) return head;
+
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+}
+// Helper function to print the linked list
+function printList(head) {
+  let curr = head;
+  let result = "";
+  while (curr !== null) {
+    result += curr.val + " -> ";
+    curr = curr.next;
+  }
+  result += "null";
+  return result;
+}
+
+// Test case 1
+const head1 = new ListNode(1);
+head1.next = new ListNode(2);
+head1.next.next = new ListNode(3);
+head1.next.next.next = new ListNode(4);
+
+console.log("Input: ", printList(head1));
+console.log("Output:", printList(reverseLinkedList(head1)));
+// Input:  1 -> 2 -> 3 -> 4 -> null
+// Output: 4 -> 3 -> 2 -> 1 -> null
