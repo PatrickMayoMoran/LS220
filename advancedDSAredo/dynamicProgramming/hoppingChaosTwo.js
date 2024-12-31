cd2c9c35d09a7e39dcb078133a3f3a0f9833380d
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-12-31 09:48:54 -0800

    Add hopping chaos iterative solution with O(1) space complexity

--- advancedDSAredo/dynamicProgramming/hoppingChaosTwo.js
+++ advancedDSAredo/dynamicProgramming/hoppingChaosTwo.js
@@ -39,16 +39,16 @@ function hoppingChaos(n) {
   if (n === 1) return 1;
   if (n === 2) return 2;
 
-  const dp = new Array(n + 1).fill(0);
-  dp[0] = 0;
-  dp[1] = 1;
-  dp[2] = 2;
+  let prev2 = 1;
+  let prev1 = 2;
 
   for (let i = 3; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
+    const sum = prev2 + prev1;
+    prev2 = prev1;
+    prev1 = sum;
   }
 
-  return dp[n];
+  return prev1;
 }
 
 
