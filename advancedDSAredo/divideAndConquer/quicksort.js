08adc5b7d50b19979bb3499c6d825a73472938d9
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-12-31 06:05:29 -0800

    Add skeleton for quicksort

---
+++ advancedDSAredo/divideAndConquer/quicksort.js
@@ -0,0 +1,31 @@
+function partition(array) {
+  const pivot = array[0];
+  let left = 1;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    while (left <= right && array[left] < pivot) {
+      left++;
+    }
+
+    while (left <= right && array[right] >= pivot) {
+      right--;
+    }
+
+    if (left < right) {
+      [array[left], array[right]] = [array[right], array[left]];
+    }
+  }
+
+  // swap pivot with right 
+  [array[0], array[right]] = [array[right], array[0]];
+
+  return array;
+}
+
+function quicksort(array) {
+}
+
+const array = [7,3,9,8,5,1];
+quicksort(array);
+console.log(array);
