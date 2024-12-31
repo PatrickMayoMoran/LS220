f5f48d060acd11b34b998dc98968d091b114c532
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-12-31 10:24:01 -0800

    Add iterative solution to chaos in the grid problem

---
+++ advancedDSAredo/dynamicProgramming/gridChaosIterative.js
@@ -0,0 +1,81 @@
+// You are given a grid represented as a nested array filled
+// with empty strings. Chaos, the puppy, is standing at the
+// top-left corner of the grid and can move either down or right
+// at any point in time. Determine the number of distinct paths
+// Chaos can take to reach a bowl of treats placed at the
+// bottom-right corner of the grid.
+
+// Define a function `chaosInTheGrid` that, given a nested
+// array, returns the number of unique paths that Chaos
+// can take to reach the bottom-right corner.
+
+// The grid will have at least 1 row and 1 column.
+
+// Example:
+
+// Given the following 2x3 grid:
+
+const grid = [
+  ["", "", ""],
+  ["", "", ""],
+];
+
+// There are three distinct path Chaos can take:
+// 1. Right -> Right -> Down
+// 2. Right -> Down -> Right
+// 3. Down -> Right -> Right
+
+function chaosInTheGrid(grid) {
+  let rows = grid.length;
+  let cols = grid[0].length;
+
+  for (let row = 0; row < rows; row++) {
+    grid[row][0] = 1;
+  }
+
+  for (let col = 0; col < cols; col++) {
+    grid[0][col] = 1;
+  }
+
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      let above = grid[i - 1][j];
+      let left = grid[i][j - 1];
+      grid[i][j] = above + left;
+    }
+  }
+
+  return grid[rows - 1][cols - 1];
+}
+
+// Test cases
+
+const grid1 = [[""]];
+const grid2 = [
+  ["", ""],
+  ["", ""],
+];
+const grid3 = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+const grid4 = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+const grid5 = [
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "", "", ""],
+];
+console.log(chaosInTheGrid(grid1) === 1);
+console.log(chaosInTheGrid(grid2) === 2);
+console.log(chaosInTheGrid(grid3) === 6);
+console.log(chaosInTheGrid(grid4) === 15);
+console.log(chaosInTheGrid(grid5) === 252);
+// All test cases should log true
