e1b45b3e87e2e27cc50c506d4c87fec5b92e7853
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-12-31 09:43:39 -0800

    Add alternative iterative solution to hopping Chaos problem

--- advancedDSAredo/dynamicProgramming/hoppingChaosTwo.js
+++ advancedDSAredo/dynamicProgramming/hoppingChaosTwo.js
@@ -36,16 +36,16 @@
 // 5. Hop 2 stacks in one go, then hop 2 stacks in one go again.
 
 function hoppingChaos(n) {
-  let sum = 0;
-  let hops = [0, 1, 2];
-  if (n <= 2) return n;
+  let dp = new Array(6);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
 
   for (let i = 3; i <= n; i++) {
-    sum = hops[i - 1] + hops[i - 2];
-    hops.push(sum);
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
 
-  return sum;
+  return dp[n];
 }
 
 
