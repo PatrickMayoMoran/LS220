ae4cb826adfbe6a2d31383d6c281c44b3febaff5
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-12-31 22:23:02 -0800

    Add iterative solution to grid chaos with cats problem

---
+++ advancedDSAredo/dynamicProgramming/iterativeCats.js
@@ -0,0 +1,87 @@
+// You are given a grid represented as a
+// nested array filled with strings.
+
+// Chaos is standing at the top-left corner of
+// the grid and can move either down or right at
+// any point in time. However, there are sleeping
+// cats in certain squares, represented by the
+// letter "C" in the grid, and Chaos cannot go through
+// these squares.
+
+// Determine the number of distinct paths Chaos
+// can take to reach a bowl of treats placed at
+// the bottom-right corner of the grid.
+
+// Define a function `chaosInTheGridWithCats` that,
+// given a nested array, returns the number of
+// unique paths that Chaos can take to reach the
+//  bottom-right corner.
+
+// The grid will have at least 1 row and 1 column.
+
+// Example:
+
+// Given the following 2x3 grid:
+
+const grid = [
+  ["", "C", ""],
+  ["", "", ""],
+];
+
+// There is only one distinct path Chaos can take:
+// 1. Down -> Right -> Right
+
+function chaosInTheGridWithCats(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (grid[row][col] === 'C') {
+        grid[row][col] = 0;
+        continue;
+      }
+      if (row === 0 && col === 0) {
+        grid[row][col] = 1;
+        continue;
+      }
+      let left = col > 0 ? grid[row][col - 1] : 0;
+      let above = row > 0 ? grid[row - 1][col] : 0;
+      grid[row][col] = left + above;
+    }
+  }
+
+  return grid[rows - 1][cols -1];
+}
+
+// Test Cases:
+
+const grid1 = [
+  ["", "C"],
+  ["", ""],
+];
+const grid2 = [["", "C"]];
+const grid3 = [
+  ["", "", ""],
+  ["", "C", ""],
+  ["", "", ""],
+];
+const grid4 = [
+  ["", "", "", "", "C"],
+  ["", "C", "", "", ""],
+  ["", "", "", "C", ""],
+];
+const grid5 = [
+  ["", "", "", "", "C", ""],
+  ["", "C", "", "", "", ""],
+  ["", "", "", "", "", ""],
+  ["", "", "", "C", "", ""],
+  ["", "C", "", "", "", ""],
+  ["", "", "", "", "", ""],
+];
+
+console.log(chaosInTheGridWithCats(grid1) === 1);
+console.log(chaosInTheGridWithCats(grid2) === 0);
+console.log(chaosInTheGridWithCats(grid3) === 2);
+console.log(chaosInTheGridWithCats(grid4) === 2);
+console.log(chaosInTheGridWithCats(grid5) === 43);
