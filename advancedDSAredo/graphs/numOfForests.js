05a47dc4e67520f30a42d2b22032db7270374c47
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-03 12:23:09 -0800

    Add recursive dfs solution to numOfForests problem

--- advancedDSAredo/graphs/numOfForests.js
+++ advancedDSAredo/graphs/numOfForests.js
@@ -9,25 +9,67 @@
 // representing the 2D map. The function should return the total
 // number of forest regions in the grid.
 
+/*
+GOAL: Count and return the number of distinct forests
+
+INPUT: grid/matrix, array of arrays
+OUTPUT: integer
+
+RULES:
+  What is distinct forest? 
+    Series of contiguous forest squares
+    forest touches on top, left, right, or down boundary, not a diagonal
+
+DATA STRCUTRES
+  Use existing grid
+
+ALGORITHM
+  Depth first search
+    If a square is forest, visit above, below, left, and right
+    Mark each square as visited
+    Increment forest count by 1
+    continue with search
+
+  When do we visit a square?
+    When it is in bounds
+      row is greater than 0 and less than rows
+      col is greater than 0 and less than cols
+*/
 function numOfForests(grid) {
   // implementation goes here
-  let numOfForests = 0;
-  const visited = new Set();
+
+  let forestCount = 0;
+  if (grid.length === 0) return forestCount;
+
   const rows = grid.length;
   const cols = grid[0].length;
+
+  function dfs(row, col) {
+    if (
+      row < 0 || row >= rows ||
+      col < 0 || col >= cols ||
+      grid[row][col] !== 'T'
+    ) {
+      return;
+    } else {
+      grid[row][col] = 'V';
+      dfs(row - 1, col); // above
+      dfs(row + 1, col); // below
+      dfs(row, col - 1); // left
+      dfs(row, col + 1); // right
+    }
+  }
 
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
       if (grid[row][col] === 'T') {
-        numOfForests += 1;
-        grid[row][col] = 'V';
-      } else {
-        continue;
+        dfs(row, col);
+        forestCount += 1;
       }
     }
   }
 
-  return numOfForests;
+  return forestCount;
 }
 
 const grid1 = [];
